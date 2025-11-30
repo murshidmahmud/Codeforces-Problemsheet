3d2b82d53a1ae14ac3250e850279b726cb8c6f40
Murshid Mahmud <murshidmahmud4@gmail.com>
2025-12-01 01:55:52 +0600

    01/12/2025

---
+++ A_Easy_Problem.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--){
+        long long n;
+        cin>>n;
+
+        int b =1;
+        int a = n-b;
+        cout<<a <<endl;
+    }
+    return 0;
+}
