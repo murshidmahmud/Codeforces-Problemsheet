bbc1cc45c6e9cea6e18fe8b49ac88641aac37e3d
Murshid Mahmud <murshidmahmud4@gmail.com>
2026-04-29 00:48:44 +0600

    29-04-2026

--- A_Word.cpp
+++ A_Word.cpp
@@ -3,6 +3,31 @@ using namespace std;
  
 int main()
 {
-    
+    string s;
+    cin>>s;
+
+    int upper = 0, lower = 0;
+
+    for(auto x:s){
+        if(isupper(x)){
+            upper++;
+        }
+        else{
+            lower++;
+        }
+    }
+
+    if(upper>lower){
+
+        for(auto& x:s){
+            x = toupper(x);
+        }
+    }
+    else{
+        for(auto& x:s){
+            x = tolower(x);
+        }
+    }
+    cout<<s;
     return 0;
 }
