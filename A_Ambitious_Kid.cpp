74b1a3aa8dd8acd28592fa73976b7a1adde7b92e
Murshid Mahmud <murshidmahmud4@gmail.com>
2026-07-06 10:37:30 +0600

    06-07-2026

---
+++ A_Ambitious_Kid.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int n;
+    cin>>n;
+
+    int ans = INT_MAX;
+    vector<int> a(n);
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+
+        ans =min(ans, abs(a[i]));
+    }
+    cout<<ans <<endl;
+    return 0;
+}
+
