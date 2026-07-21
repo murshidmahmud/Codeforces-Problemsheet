7104a51adcf2971da2217d978431dc3a0b5a84b4
Murshid Mahmud <murshidmahmud4@gmail.com>
2026-07-22 00:09:23 +0600

    22-07-2026

---
+++ A_Brain_s_Photos.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    char a[n][m];
+
+    bool color = false;
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> a[i][j];
+
+            if (a[i][j] == 'C' || a[i][j] == 'M' || a[i][j] == 'Y')
+            {
+                color = true;
+            } 
+        }
+    }
+
+    if(color) cout<<"#Color \n";
+    else cout<<"#Black&White \n";
+
+    return 0;
+}
